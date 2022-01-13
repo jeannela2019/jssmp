d549a54408b2f81d0ccc76ddd91e1f6b4faf9624
jeannela2019 <4545580+jeannela2019@users.noreply.github.com>
2022-01-13 15:46:17 +0800

    proj add some files, some plan

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,10 @@
-module.exports = {
-  input: './src/mainjs.js',
+module.exports = [{
+  input: './src/jssplaylist.js',
   output: [{
-    file: 'dist/index.js',
+    file: 'dist/import_jssplaylist.js',
     format: 'cjs',
     sourcemap: false,
-  }, {
-    //file: 'dist/index.es.js',
-    //format: 'es',
-    //sourcemap: true
   }],
   plugins: [require('@rollup/plugin-buble')()],
   external(id) { return id[0] != "." && !require("path").isAbsolute(id) }
-}
+}];
---
+++ src/common/color.js
@@ -0,0 +1,1 @@
+// Color procedures.
